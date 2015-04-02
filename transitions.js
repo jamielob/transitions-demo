ae66f2d520ca10684b8c657de7e80cd948e6e1a5
Jamie Loberman <jamie@beautifulcoding.com>
2015-04-02 13:25:22 -0700

    V2: Major update

--- transitions.js
+++ transitions.js
@@ -1,11 +1,32 @@
 if (Meteor.isClient) {
   
+  Session.setDefault('dynamicTemplate', 'test1' );
+
   Router.configure({layoutTemplate: 'layout'});
   Router.route('/', { name: 'page1' });
   Router.route('/page2', { name: 'page2' });
 
-  //Transitions.container = '#custom-container';
+  //Transitions.layout = 'custom-layout';
+  //Transitions.container = '.custom-container';
   //Transitions.content = '.custom-content';
+  Transitions.transitionOut = 'fadeOut'
+  Transitions.transitionIn = 'fadeIn'
+
+  Template.page2.events({
+  	'click .toggleTest': function () {
+  		if (Session.get('dynamicTemplate') === 'test2') {
+  			Session.set('dynamicTemplate', 'test1' );
+  		} else {
+  			Session.set('dynamicTemplate', 'test2' );
+  		}
+  	}
+  });
+
+  Template.page2.helpers({
+  	dynamicTemplate: function () {
+  		return Session.get('dynamicTemplate');
+  	}
+  });
 
 
 }
